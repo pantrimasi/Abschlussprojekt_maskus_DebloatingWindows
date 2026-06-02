9efde3a6ca71efe7be0627c85666c6a6e85fdecc
Masato Kuster <pantrimasi@gmail.com>
2026-06-02 11:25:09 +0200

    Add function to disable ads and suggestions in Windows
    This function disables various advertising features in Windows by modifying registry settings.

--- Debloater/Modules/Privacy.ps1
+++ Debloater/Modules/Privacy.ps1
@@ -1,1 +1,16 @@
+function Disable-Ads {
+    # Disable ads ID
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
 
+    # Disable lockscreen tips
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338387Enabled /t REG_DWORD /d 0 /f
+
+    # Disable suggested apps
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338388Enabled /t REG_DWORD /d 0 /f
+
+    # Disable start menu suggestions
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
+
+    # Disable Bing suggestions
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SearchSettings" /v IsDynamicSearchBoxEnabled /t REG_DWORD /d 0 /f
+}
